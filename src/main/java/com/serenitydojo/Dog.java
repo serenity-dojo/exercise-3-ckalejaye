e4be7704941a69669b1b737a83fd75a5df4ef640
Cynthia Kalejaye <ckalejaye@gmail.com>
2022-10-08 18:15:52 +0100

    Exercise3.1

---
+++ src/main/java/com/serenitydojo/Dog.java
@@ -0,0 +1,38 @@
+package com.serenitydojo;
+
+public class Dog {
+    public String name;
+
+    public String favouriteFood;
+
+    public int age;
+
+    public Dog(String fido, String bone, int i) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    public void setFavouriteFood(String favouriteFood) {
+        this.favouriteFood = favouriteFood;
+    }
+
+    public int getAge() {
+        return age;
+
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -3,5 +3,31 @@ package com.serenitydojo;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class WhenCreatingObjects {
+
+    @Test
+
+    public void creating_a_dog(){
+/*        Dog fido = new Dog("Fido", "Bone", 5);
+
+
+        assertEquals(fido.getName(), "Fido");
+        assertEquals(fido.getFavouriteFood(), "Bone");
+        assertEquals(fido.getAge(), 5);
+*/
+
+        String name ="Fido";
+        String favouriteFood = "Bone";
+        int age = 5;
+
+        Dog fido = new Dog("name","favouriteFood", 5);
+
+        fido.name = "Fido";
+        fido.favouriteFood = "Bone";
+        fido.age = 5;
+
+        }
+
 }
